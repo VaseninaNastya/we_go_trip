f6be89fb1137859c4880cb530ad48a9086191c6c
VaseninaNastya <a.vasenina@andersenlab.com>
2022-03-14 09:24:54 +0300

    fix: fix build

---
+++ src/components/GraphsContainer/GraphsContainer.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Wrapper, GraphsWrapper } from "./GraphsContainer.elements";
+import DatePickersContainer from "../DatePickersContainer/DatePickersContainer";
+import GraphAndIndicators from "./GraphAndIndicators/GraphAndIndicators";
+import { datePickerСonst } from "../../constants/datePickerСonst";
+import filterData from "./filterData";
+import getClickPercent from "./getClickPercent";
+import getSalesPercent from "./getSalesPercent";
+import { titlesСonst } from "../../constants/titlesСonst";
+
+import getData from "../../servises/getData";
+import { useQuery } from "react-query";
+
+const GraphsContainer = () => {
+  const { maxDate } = datePickerСonst;
+  const [dates, setDates] = useState([null, maxDate]);
+  const [startDates, endDates] = dates;
+
+  const { data, isSuccess } = useQuery("res", () => getData());
+
+  const { purchases, views_to_clicks } = data;
+  const clickPercent = getClickPercent(views_to_clicks);
+
+  const purchasesData = filterData(purchases, dates);
+  const viewAndClicksData = filterData(clickPercent, dates);
+  const salesPercent = getSalesPercent(purchases);
+  const salesPercentData = filterData(salesPercent, dates);
+  const { sales, balance, click, views } = titlesСonst;
+
+  return (
+    isSuccess && (
+      <Wrapper>
+        <DatePickersContainer dates={dates} setDates={setDates} />
+        {startDates && endDates && (
+          <GraphsWrapper>
+            <GraphAndIndicators
+              data={purchasesData}
+              dates={dates}
+              title={sales}
+            />
+            <GraphAndIndicators
+              data={purchasesData}
+              dates={dates}
+              title={balance}
+            />
+            <GraphAndIndicators
+              data={viewAndClicksData}
+              dates={dates}
+              title={views}
+            />
+            <GraphAndIndicators
+              data={salesPercentData}
+              dates={dates}
+              title={click}
+            />
+          </GraphsWrapper>
+        )}
+      </Wrapper>
+    )
+  );
+};
+
+export default GraphsContainer;
